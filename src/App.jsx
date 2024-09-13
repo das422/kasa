630d667f56a48edceb23068871a2cdb25a4721c0
das422 <simonin974@gmail.com>
2024-09-13 20:23:32 +0400

    fix_colorText_notFound

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./utils/Routes/routes";
 
-
 function App() {
-	return (
-		<>
-			<RouterProvider router={router}/>
-		</>
-	);
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
--- src/components/collapse/Collapse.jsx
+++ src/components/collapse/Collapse.jsx
@@ -1,33 +1,32 @@
+import "./collapse.scss";
 
-import './collapse.scss'
+import arrow from "@/assets/arrow.png";
+import { useState } from "react";
 
-import arrow from '@/assets/arrow.png';
-import { useState } from 'react';
+export default function Collapse({ title, content }) {
+  const [toggle, setToggle] = useState(false);
 
-export default function Collapse({title, content}) {
-
-    const [toggle, setToggle] = useState(false);
-
-    return (
-        <>
-            <div className="collapse" >
-                <h3 className='collapse_title' onClick={() => setToggle(!toggle)} >
-                    {title}
-                    <img 
-                        className={toggle ? 'arrow arrow_up' : 'arrow arrow_down'} 
-                        src={arrow} 
-                        alt="show content" 
-                    />
-                </h3>
-                <div className={toggle ? 'collapse_content' : 'collapse_content_hidden'}>
-                    {Array.isArray(content) ? content.map((item, index) => {
-                        return (
-                            <p key={index}>{item}</p>
-                        )
-                    }) : content
-                    }
-                </div> 
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="collapse">
+        <h3 className="collapse_title" onClick={() => setToggle(!toggle)}>
+          {title}
+          <img
+            className={toggle ? "arrow arrow_up" : "arrow arrow_down"}
+            src={arrow}
+            alt="show content"
+          />
+        </h3>
+        <div
+          className={toggle ? "collapse_content" : "collapse_content_hidden"}
+        >
+          {Array.isArray(content)
+            ? content.map((item, index) => {
+                return <p key={index}>{item}</p>;
+              })
+            : content}
+        </div>
+      </div>
+    </>
+  );
 }
